e14dcdf5732dbea29ba2d30562a90ddd70284312
HowdoIC0DE <137148316+HowdoIC0DE@users.noreply.github.com>
2026-05-08 22:01:44 +0200

    TransactionList erstellt

---
+++ src/components/TransactionList.vue
@@ -0,0 +1,34 @@
+<script setup>
+const transactions = [
+  {
+    id: 1,
+    title: "Miete",
+    amount: 650.0,
+    type: "EXPENSE"
+  },
+  {
+    id: 2,
+    title: "Gehalt",
+    amount: 1400.0,
+    type: "INCOME"
+  },
+  {
+    id: 3,
+    title: "Einkauf",
+    amount: 45.99,
+    type: "EXPENSE"
+  }
+]
+</script>
+
+<template>
+  <section>
+    <h2>Transactions</h2>
+
+    <ul>
+      <li v-for="transaction in transactions" :key="transaction.id">
+        {{ transaction.title }} - {{ transaction.amount }} € - {{ transaction.type }}
+      </li>
+    </ul>
+  </section>
+</template>
