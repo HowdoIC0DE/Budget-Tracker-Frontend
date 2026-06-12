58740e0b2fd93a0f313991e8c62534678cdae271
HowdoIC0DE <137148316+HowdoIC0DE@users.noreply.github.com>
2026-06-12 21:33:25 +0200

    updated app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,5 @@
 <script setup>
+import TransactionForm from './components/TransactionForm.vue'
 import TransactionList from './components/TransactionList.vue'
 import CategoryList from './components/CategoryList.vue'
 </script>
@@ -7,6 +8,7 @@ import CategoryList from './components/CategoryList.vue'
   <main>
     <h1>Budget Tracker</h1>
 
+    <TransactionForm />
     <TransactionList />
     <CategoryList />
   </main>
